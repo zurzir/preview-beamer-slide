04b7505dd5616d4d29a94c1f47d1b35e6d61b793
lehilton <lehilton@gmail.com>
2019-02-14 19:37:50 -0200

    hack_synctex

--- lib/previewframe.py
+++ lib/previewframe.py
@@ -114,7 +114,7 @@ def extract_frame(lines, linenum, nbefore, nafter, include_surroundings):
             end_line = i - 1
             break
 
-    return "".join(lines[begin_line:end_line + 1]), begin_line
+    return "".join(lines[begin_line:end_line + 1]), begin_line, end_line
 
 
 def extract_header(lines, linenum):
@@ -134,52 +134,61 @@ def extract_header(lines, linenum):
     return h
 
 def create_prevfile(args):
+    tex_lines = read_lines(args.texfile)
+    dirpath = os.path.dirname(args.texfile)
+
+    # best guess for mainfile canditate
+    if args.mainfile:
+        candidate = args.mainfile
+        candidate_lines = read_lines(candidate)
+    else:
+        candidate = args.texfile
+        candidate_lines = tex_lines
+
+    p, p_file = find_preamble(dirpath, candidate, candidate_lines)
+
+    r, first_frame_line, last_frame_line = extract_frame(tex_lines, args.linenum, args.nbefore, args.nafter, args.include_surroundings)
+    h = '' if p_file == args.texfile else extract_header(tex_lines, first_frame_line)
+
+    contents = args.before_preample
+    contents += p
+    contents += "\\begin{document}\n"
+    contents += h
+    if args.hack_synctex:
+        cur_line = contents.count('\n')
+        add_lines = 2*first_frame_line - last_frame_line - cur_line + 3
+        if add_lines < 0:
+            add_lines = 0
+        contents += '\n' * add_lines
+    contents += r
+    contents += "\\end{document}\n"
 
     with open(TEMP_FILE, 'w') as f:
-        tex_lines = read_lines(args.texfile)
-        dirpath = os.path.dirname(args.texfile)
-
-        # best guess for mainfile canditate
-        if args.mainfile:
-            candidate = args.mainfile
-            candidate_lines = read_lines(candidate)
-        else:
-            candidate = args.texfile
-            candidate_lines = tex_lines
-
-        p, p_file = find_preamble(dirpath, candidate, candidate_lines)
-
-        r, first_frame_line = extract_frame(tex_lines, args.linenum, args.nbefore, args.nafter, args.include_surroundings)
-        h = '' if p_file == args.texfile else extract_header(tex_lines, first_frame_line)
-
-        f.write(args.before_preample)
-        f.write(p)
-        f.write("\\begin{document}\n")
-        f.write(h)
-        f.write(r)
-        f.write("\\end{document}\n")
-
+        f.write(contents)
 
 def main():
     parser = argparse.ArgumentParser(description='Prevê um frame do beamer.')
     parser.add_argument('-l', dest='linenum', required=True, type=int, help='número da linha do arquivo')
     parser.add_argument('-t', dest='texfile', required=True, help='nome do arquivo')
-    parser.add_argument('-m', dest='mainfile', default=None, help='nome do arquivo do preâmbulo ou\
-                        nenhum para usar o mesmo arquivo do frame; se não encontrar nas primeiras\
-                        linhas, então procura por arquivos .tex que tenham \\documentclass e estejam\
-                        no mesmo diretorio')
+    parser.add_argument('-m', dest='mainfile', default=None,
+                        help='''nome do arquivo do preâmbulo ou
+                                nenhum para usar o mesmo arquivo do frame; se não encontrar nas primeiras
+                                linhas, então procura por arquivos .tex que tenham \\documentclass e estejam
+                                no mesmo diretorio''')
     parser.add_argument('-a', dest='nbefore', type=int, default=0, help='número da frames antes')
     parser.add_argument('-d', dest='nafter', type=int, default=0, help='número da frames depois')
     parser.add_argument('-s', dest='include_surroundings', action='store_false',
-                              help='Desabilita a includsão dos arredores dos frames \
-                              (normalmente usados para comandos gerais; os arredores \
-                              podem ser delimitados por comentários que começãom com %%')
+                        help='''Desabilita a includsão dos arredores dos frames
+                                (normalmente usados para comandos gerais; os arredores
+                                podem ser delimitados por comentários que começãom com %%''')
     parser.add_argument('-p', dest='previewer', default='evince', help='visualizador do pdf (argumentos separados por espaço)')
-    parser.add_argument('-c', dest='compiler', default='pdflatex -interaction=nonstopmode -shell-escape', help='copilador do pdf (argumentos separados por espaço)')
+    parser.add_argument('-c', dest='compiler', default='pdflatex -synctex=1 -interaction=nonstopmode -shell-escape',
+                        help='copilador do pdf (argumentos separados por espaço)')
     parser.add_argument('-n', dest='nopreview', action='store_true', help='desabilita visualização de pdf')
+    parser.add_argument('-x', dest='hack_synctex', action='store_true', help='habilita hack do synctex para voltar ao arquivo original')
     parser.add_argument('-b', dest='before_preample', default='',
-                              help='Concatena antes do preâmbulo; útil para passar \
-                              argumentos para pacotes: e.g.: \\PassOptionsToClass{handout}{beamer}')
+                        help='''Concatena antes do preâmbulo; útil para passar
+                                argumentos para pacotes: e.g.: \\PassOptionsToClass{handout}{beamer}''')
     args = parser.parse_args()
 
     # linha começa do zero no vetor do python
@@ -191,7 +200,16 @@ def main():
 
     create_prevfile(args)
     subprocess.check_call(compiler_command + [TEMP_FILE])
+    if args.hack_synctex:
+        subprocess.check_call(['gunzip', 'beamerprevframe.synctex.gz'])
+        sed_exp = 's#^Input:1:.*#Input:1:{}#'.format(os.path.realpath(args.texfile))
+        subprocess.check_call(['sed', '-re', sed_exp, '-i', 'beamerprevframe.synctex'])
+        subprocess.check_call(['gzip', 'beamerprevframe.synctex.gz'])
+
     if not args.nopreview:
         subprocess.Popen(previewer_command + [TEMP_PDF])
+
+    # se não faz preview, então faz um hack no synctex.gz para direcionar para
+    # o arquivo correto
 
 main()
